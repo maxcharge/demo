a6ba4d24dbef57bd7e26ce46c95cda3cfe6943d1
Mayur Jain <maxcharge788769@gmail.com>
2021-07-15 08:39:45 +0530

    Updates

--- src/L2/MyComponent/shopclues/MyComponent.jsx
+++
@@ -1,7 +0,0 @@
-import MyComponentL1 from '../../../L1/MyComponent';
-
-const MyComponentL2 = (props) => {
-    return (<MyComponentL1 {...props} client={"shopclues"} ></MyComponentL1>);
-};
-
-export default MyComponentL2;
